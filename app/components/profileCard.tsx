7d647042e67d07f80ce5fec68262f332a999acb9
Thomas <thomas_mcardle@outlook.com>
2026-04-08 10:32:25 +0100

    Added basic profile picture placeholder.

--- app/components/profileCard.tsx
+++ app/components/profileCard.tsx
@@ -2,6 +2,7 @@ export default function ProfileCard({ userId } : any) {
   return (
     <a className="w-full" href={`/profiles/${userId}`} >
         <div className="flex flex-row items-center flex-1 w-full rounded-lg border border-zinc-200 p-4 dark:border-zinc-700 gap-2">
+            <img src="https://placehold.co/80x80" alt="Profile Picture" className="rounded-full w-20 h-20" />
             <div className="flex flex-col">
                 <h2 className="font-semibold">{'John Doe'}</h2>
                 <p className="text-sm text-zinc-600 dark:text-zinc-400">{'Data analyst'}</p>
