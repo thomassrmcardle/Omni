e8b41ad92ae2f2c8201d26c815354841faf44ba1
Thomas <thomas_mcardle@outlook.com>
2026-04-08 11:47:11 +0100

    Improved profile page.

--- app/app/profiles/[id]/page.tsx
+++ app/app/profiles/[id]/page.tsx
@@ -19,12 +19,12 @@ export async function generateMetadata({ params }: Props) {
 export default function UserPage({ params }: Props) {
 
   function MainArea() {
-    return <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    return <div className="flex flex-col flex-1 w-full max-w-3xl items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
             <div>
                 <h1 className="text-2xl font-bold">{getUser(params.id).name}</h1>
                 <p className="text-zinc-600 dark:text-zinc-400">
-                  Contributor
+                  Contributor | Data Analyst
                 </p>
             </div>
             <h2 className="text-xl font-semibold mt-8">Recent Activity</h2>
