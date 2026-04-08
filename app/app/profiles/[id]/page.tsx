5acb9be796892d3323972fe3e3a0e534f80aaded
Thomas <thomas_mcardle@outlook.com>
2026-04-08 11:43:32 +0100

    Made some improvements to the profile page design; added space for stats and updated placeholders.

--- app/app/profiles/[id]/page.tsx
+++ app/app/profiles/[id]/page.tsx
@@ -17,12 +17,14 @@ export async function generateMetadata({ params }: Props) {
 }
 
 export default function UserPage({ params }: Props) {
-  return <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+
+  function MainArea() {
+    return <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
             <div>
-                <h1 className="text-2xl font-bold">Profile of {getUser(params.id).name}</h1>
-                <p className="text-zinc-600 dark:text-zinc-400 mt-4">
-                This is the profile page for user with ID: {params.id}.
+                <h1 className="text-2xl font-bold">{getUser(params.id).name}</h1>
+                <p className="text-zinc-600 dark:text-zinc-400">
+                  Contributor
                 </p>
             </div>
             <h2 className="text-xl font-semibold mt-8">Recent Activity</h2>
@@ -39,4 +41,21 @@ export default function UserPage({ params }: Props) {
   
         </main>
       </div>
+  }
+
+  function StatsArea() {
+    return <div className="flex flex-col flex-1 w-full max-w-lg justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex flex-1 flex-col py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <h1 className="text-2xl font-bold">User Statistics</h1>
+          <p className="text-zinc-600 dark:text-zinc-400 mt-4">
+            This section provides an overview of the user's statistics, including their activity level, contributions, and engagement with the platform.
+          </p>
+        </main>
+      </div>
+  }
+
+  return <div className="flex flex-row">
+    <MainArea />
+    <StatsArea />
+  </div>
 }
