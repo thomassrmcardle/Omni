b0eb9420abe4e3a97d42a61b4ebc07dd71415181
Thomas <thomas_mcardle@outlook.com>
2026-04-08 08:33:55 +0100

    Fixed profile card sizing.

--- app/components/profileCard.tsx
+++ app/components/profileCard.tsx
@@ -1,10 +1,12 @@
 export default function ProfileCard({ userId } : any) {
   return (
     <a href={`/profiles/${userId}`} >
-      <div className="flex flex-col gap-2 rounded-lg border border-zinc-200 p-4 dark:border-zinc-700">
-        <h2 className="text-l font-semibold">{'John Doe'}</h2>
-        <p className="text-zinc-600 dark:text-zinc-400">{'Data analyst'}</p>
-      </div>
+        <div className="flex flex-1">
+            <div className="flex flex-col gap-2 rounded-lg border border-zinc-200 p-4 dark:border-zinc-700">
+                <h2 className="font-semibold">{'John Doe'}</h2>
+                <p className="text-sm text-zinc-600 dark:text-zinc-400">{'Data analyst'}</p>
+            </div>
+        </div>
     </a>
   );
 }
