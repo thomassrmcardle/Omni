2706d508baa4b9d7c416723eab75dbb80f31c4de
Thomas <thomas_mcardle@outlook.com>
2026-04-09 11:40:25 +0100

    Improved sign up page clarity.

--- app/app/signup/page.tsx
+++ app/app/signup/page.tsx
@@ -29,17 +29,17 @@ export default function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
         <div className="w-full max-w-md bg-white dark:bg-black rounded-lg shadow-md p-8 justify-center">
 
-          <h2>Sign Up</h2>
+          <h1 className="text-2xl font-bold mb-4">Sign Up</h1>
 
           <input type="email" placeholder="Email" value={input_email} onChange={(e) => setInputEmail(e.target.value)} className="w-full border border-zinc-300 rounded-md p-2 mb-4" />
           <input type="password" placeholder="Password" value={input_password} onChange={(e) => setInputPassword(e.target.value)} className="w-full border border-zinc-300 rounded-md p-2 mb-4" />
           
-          <div>
-            <input type="radio" id="remember" name="remember" checked={termsAccepted} onChange={(e) => setTermsAccepted(e.target.checked)} className="mb-4" />
-            <label htmlFor="remember" className="ml-2 text-sm text-gray-600 dark:text-gray-400">I agree to the Terms and Conditions, Privacy Policy and Community Guidelines</label>
+          <div className="flex flex-row gap-2">
+            <input type="radio" id="remember" name="remember" checked={termsAccepted} onChange={(e) => setTermsAccepted(!termsAccepted)} className="mb-4" />
+            <p className="ml-2 text-sm text-gray-600 dark:text-gray-400">I agree to the Terms and Conditions & Privacy Policy</p>
           </div>
 
           <button className="w-full bg-blue-500 text-white rounded-md p-2" onClick={() => CreateAccount()} disabled={!termsAccepted}>
