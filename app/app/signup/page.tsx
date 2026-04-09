3b0962bcdb1fbbe1e2e56b0bb7160b1ebcf888a7
Thomas <thomas_mcardle@outlook.com>
2026-04-09 11:53:11 +0100

    Added links to policies inside signup.

--- app/app/signup/page.tsx
+++ app/app/signup/page.tsx
@@ -30,17 +30,25 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
-        <div className="w-full max-w-md bg-white dark:bg-black rounded-lg shadow-md p-8 justify-center">
+        <div className="w-full max-w-md card shadow-md p-8 justify-center">
 
           <h1 className="text-2xl font-bold mb-4">Sign Up</h1>
 
           <input type="email" placeholder="Email" value={input_email} onChange={(e) => setInputEmail(e.target.value)} className="w-full border border-zinc-300 rounded-md p-2 mb-4" />
           <input type="password" placeholder="Password" value={input_password} onChange={(e) => setInputPassword(e.target.value)} className="w-full border border-zinc-300 rounded-md p-2 mb-4" />
           
-          <div className="flex flex-row gap-2">
-            <input type="radio" id="remember" name="remember" checked={termsAccepted} onChange={(e) => setTermsAccepted(!termsAccepted)} className="mb-4" />
-            <p className="ml-2 text-sm text-gray-600 dark:text-gray-400">I agree to the Terms and Conditions & Privacy Policy</p>
+          <div className="flex flex-row gap-2 items-center mb-4">
+            <input type="radio" id="remember" name="remember" checked={termsAccepted} onChange={(e) => setTermsAccepted(e.target.checked)} className="mb-4" />
+            <p className="ml-2 text-sm text-gray-600 dark:text-gray-400">I agree to the
+              <a href="/terms" className="text-blue-500 hover:underline">Terms and Conditions</a>
+              &
+              <a href="/privacy" className="text-blue-500 hover:underline">Privacy Policy</a>
+            </p>
           </div>
+
+          <p className="text-sm text-gray-600 dark:text-gray-400 mb-4">
+            All users will also be expected to abide by the <a href="/guidelines" className="text-blue-500 hover:underline">Community Guidelines</a>, designed to ensure safety ande respect. Failure to comply with these guidelines may result in account suspension or termination.
+          </p>
 
           <button className="w-full bg-blue-500 text-white rounded-md p-2" onClick={() => CreateAccount()} disabled={!termsAccepted}>
             Sign Up
