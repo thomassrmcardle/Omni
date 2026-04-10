8ff36b364bf915fc78f9fd4da74888eb458b4fcf
Thomas <thomas_mcardle@outlook.com>
2026-04-10 01:28:14 +0100

    Fixed parameter issue.

--- app/app/profiles/[id]/page.tsx
+++ app/app/profiles/[id]/page.tsx
@@ -2,7 +2,7 @@ import ArticleCard from "@/components/articleCard";
 import VerifyEmailPrompt from "@/components/prompts/verifyEmail";
 
 interface Props {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 
@@ -10,8 +10,52 @@ function getUser(id: string) {
     return { name: "John Doe" }; // Mock user data, replace with actual data fetching logic
 }
 
+function MainArea({ id }: { id: string }) {
+  return <div className="flex flex-col flex-1 w-full max-w-3xl items-center justify-center">
+      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <div className="flex flex-row items-center gap-2">
+            <img src="https://placehold.co/56x56" alt="Profile Picture" className="rounded-full" />
+            <div>
+                <h1 className="text-2xl font-bold">{getUser(id).name}</h1>
+                <p className="text-zinc-600 dark:text-zinc-400">
+                  Contributor | Data Analyst
+                </p>
+            </div>
+          </div>
+
+          <p>{"ID: " + id}</p>
+
+          <h2 className="text-xl font-semibold mt-8">Recent Activity</h2>
+          <VerifyEmailPrompt profileId={id} />
+          <div className="mt-8 grid w-full gap-4">
+              <ArticleCard 
+              title="User&apos;s Recent Activity" 
+              description="This section shows the recent activity of the user, including articles they&apos;ve interacted with and comments they&apos;ve made." 
+              />
+              <ArticleCard 
+              title="User&apos;s Contributions" 
+              description="This section highlights the contributions made by the user, such as articles they&apos;ve written or projects they&apos;ve participated in." 
+              />
+          </div>
+
+      </main>
+    </div>
+}
+
+function StatsArea() {
+  return <div className="flex flex-col flex-1 w-full max-w-lg justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 flex-col py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-2xl font-bold">User Statistics</h1>
+        <p className="text-zinc-600 dark:text-zinc-400 mt-4">
+          This section provides an overview of the user's statistics, including their activity level, contributions, and engagement with the platform.
+        </p>
+      </main>
+    </div>
+}
+
 export async function generateMetadata({ params }: Props) {
-  const user = await getUser(params.id); 
+  const { id } = await params;
+  const user = await getUser(id); 
 
   return {
     title: (user ? user.name : "User Profile"),
@@ -21,58 +65,13 @@ export async function generateMetadata({ params }: Props) {
   };
 }
 
-export default function UserPage({ params }: { params: { id: string } }) {
-
-  console.log(params)
-  console.log(params.id)
-
-  function MainArea({ id }: { id: string }) {
-    return <div className="flex flex-col flex-1 w-full max-w-3xl items-center justify-center">
-        <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-            <div className="flex flex-row items-center gap-2">
-              <img src="https://placehold.co/56x56" alt="Profile Picture" className="rounded-full" />
-              <div>
-                  <h1 className="text-2xl font-bold">{getUser(id).name}</h1>
-                  <p className="text-zinc-600 dark:text-zinc-400">
-                    Contributor | Data Analyst
-                  </p>
-              </div>
-            </div>
-
-            <p>{"ID: " + id}</p>
-
-            <h2 className="text-xl font-semibold mt-8">Recent Activity</h2>
-            <VerifyEmailPrompt profileId={id} />
-            <div className="mt-8 grid w-full gap-4">
-                <ArticleCard 
-                title="User's Recent Activity" 
-                description="This section shows the recent activity of the user, including articles they've interacted with and comments they've made." 
-                />
-                <ArticleCard 
-                title="User's Contributions" 
-                description="This section highlights the contributions made by the user, such as articles they've written or projects they've participated in." 
-                />
-            </div>
-  
-        </main>
-      </div>
-  }
-
-  function StatsArea() {
-    return <div className="flex flex-col flex-1 w-full max-w-lg justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex flex-1 flex-col py-32 px-16 bg-white dark:bg-black sm:items-start">
-          <h1 className="text-2xl font-bold">User Statistics</h1>
-          <p className="text-zinc-600 dark:text-zinc-400 mt-4">
-            This section provides an overview of the user's statistics, including their activity level, contributions, and engagement with the platform.
-          </p>
-        </main>
-      </div>
-  }
+export default async function Page({ params }: { params: Promise<{ id: string }>; }) {
+  const { id } = await params;
 
   return <div>
-    <p>{"SOURCE ID: " + params.id}</p>
+    <p>{"SOURCE ID: " + id}</p>
     <div className="flex flex-row w-full items-start justify-center bg-zinc-50 font-sans dark:bg-black">
-      <MainArea id={params.id} />
+      <MainArea id={id} />
       <StatsArea />
     </div>
   </div>
