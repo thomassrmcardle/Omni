795148025b50057f437d2029d18156944477613b
Thomas <thomas_mcardle@outlook.com>
2026-04-10 14:39:29 +0100

    Added option to close email prompt.

--- app/components/prompts/verifyEmail.tsx
+++ app/components/prompts/verifyEmail.tsx
@@ -44,6 +44,10 @@ export default function VerifyEmailPrompt({profileId}: { profileId: string }) {
         router.push("/verify-email");
     }
 
+    function closePrompt() {
+        setVisible(false);
+    }
+
     if (isSelf && isUnverified && visible) {
         return (
             <div className="flex flex-col items-center justify-center gap-4 w-full bg-blue-50 dark:bg-blue-900 rounded-lg p-6 border border-blue-200 dark:border-blue-700 mb-8">
@@ -55,7 +59,7 @@ export default function VerifyEmailPrompt({profileId}: { profileId: string }) {
                     <button onClick={openPrompt} className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 w-full text-center">
                         Send Email
                     </button>
-                    <button className="w-full bg-zinc-200 dark:bg-zinc-700 rounded-md px-4 py-2 hover:bg-zinc-300 dark:hover:bg-zinc-600 text-center">
+                    <button onClick={closePrompt} className="w-full bg-zinc-200 dark:bg-zinc-700 rounded-md px-4 py-2 hover:bg-zinc-300 dark:hover:bg-zinc-600 text-center">
                         Maybe later
                     </button>
                 </div>
