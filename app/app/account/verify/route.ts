5d91f8e332e4cebac5b0cde7d86305fd241151ae
Thomas <thomas_mcardle@outlook.com>
2026-04-14 00:39:04 +0100

    Removed potential error.

--- app/app/account/verify/route.ts
+++ app/app/account/verify/route.ts
@@ -34,7 +34,6 @@ export async function GET(req: Request) {
         })
         .eq('id', data.id)
         .select()
-        .single()
 
     if (updateError) {
         console.error(updateError);
