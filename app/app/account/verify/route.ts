ba58c2980eb37812efd303259e1d9902715f4b49
Thomas <thomas_mcardle@outlook.com>
2026-04-14 00:38:34 +0100

    Added checks for server processes for verification.

--- app/app/account/verify/route.ts
+++ app/app/account/verify/route.ts
@@ -16,14 +16,16 @@ export async function GET(req: Request) {
         .select('*')
         .eq('email_verify_token', token)
         .single()
+    
+    if (error || !data) return new Response("Invalid", {status: 400});
 
     if (new Date(data.email_verify_expires_at) < new Date()) {
         return new Response("Token expired", { status: 400 });
     }
-    
-    if (error || !data) return new Response("Invalid", {status: 400});
 
-    const { error: updateError } = await supabaseAdmin
+    console.log('Found user', data.id)
+
+    const { data: updatedData, error: updateError } = await supabaseAdmin
         .from('profiles')
         .update({
             email_verified: true,
@@ -38,6 +40,8 @@ export async function GET(req: Request) {
         console.error(updateError);
         return new Response("Failed to update", { status: 500 });
     }
+
+    console.log('Updated', updatedData)
 
     return Response.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/account/verify-success`);
 }
