73b4b10b945f31b0c7cbbdd41647b3f0a4d2c1fe
Thomas <thomas_mcardle@outlook.com>
2026-04-13 13:32:16 +0100

    Tried to resolve type issue.

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createBrowserClient(supabaseUrl, supabaseKey, {
     auth: {
