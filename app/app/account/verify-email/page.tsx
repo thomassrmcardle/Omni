a50617b2b9893313a5c09cf9fc9ad9ffe6d4c07d
Thomas <thomas_mcardle@outlook.com>
2026-04-13 22:49:54 +0100

    Fixed glitch with resend ux issue.

--- app/app/account/verify-email/page.tsx
+++ app/app/account/verify-email/page.tsx
@@ -12,7 +12,7 @@ type SupabaseUser = {
 
 export default function VerifyMessageScreen() {
 
-    const [cooldown, setCooldown] = useState(0)
+    const [cooldown, setCooldown] = useState(30)
     const [email, setEmail] = useState<string | null>(null);
 
     useEffect(() => {
@@ -24,10 +24,6 @@ export default function VerifyMessageScreen() {
 
         return () => clearInterval(timer);
     }, [cooldown]);
-
-    useEffect(() => {
-        setCooldown(30)
-    }, [])
     
     useEffect(() => {
         supabase.auth.getUser().then(({ data }: any) => {
@@ -55,6 +51,16 @@ export default function VerifyMessageScreen() {
         }
     }
 
+
+    function ResendEmail() {
+
+
+        setCooldown(30)
+
+        // Some typa logic here
+
+    } 
+
     return <div className="bg-white dark:bg-black py-32 px-16 w-full" >
         <div className="flex flex-col items-center justify-center">
             <div className="card shadow-md p-8 justify-center max-w-lg">
@@ -62,8 +68,8 @@ export default function VerifyMessageScreen() {
                 <h2 className="w-full font-bold text-xl mt-8">Check Your Email</h2>
                 <p className="w-full mt-2">We've sent you an email with a link to verify your email address. Please check your inbox and click the link to complete the verification process. It may be in your spam folder.</p>
 
-                <button disabled={cooldown > 0} className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 text-center w-full mt-4">
-                    {cooldown > 0 ? `Resend in ${cooldown}s` : "Resend Email"}
+                <button onClick={ResendEmail} disabled={cooldown > 0} className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 text-center w-full mt-4">
+                    {cooldown > 0 ? `Try again in ${cooldown}s` : "Resend Email"}
                 </button>
 
                 <p className="w-full text-zinc-600 dark:text-zinc-400 text-center mt-4">Need to change your email? <a href="/account/settings" className="text-blue-500 hover:underline">Open Account Settings</a>.</p>
