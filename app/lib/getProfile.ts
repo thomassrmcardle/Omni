e0aa531b76006ecc1c054b7aabf5a542228308b6
Thomas <thomas_mcardle@outlook.com>
2026-04-13 12:05:13 +0100

    Attempted to fix api registry.

---
+++ app/lib/getProfile.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/lib/supabaseClient";
+
+export default async function getProfile(userId: string, retries = 3) {
+  if (!userId) return null;
+
+  const { data } = await supabase
+    .from("public_profiles")
+    .select("*")
+    .eq("id", userId)
+    .maybeSingle();
+
+  return data || null;
+}
