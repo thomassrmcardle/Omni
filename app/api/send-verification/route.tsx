50ca6c64cb1d926fa70898f80cf62a2cf678cafe
Thomas <thomas_mcardle@outlook.com>
2026-04-13 13:52:19 +0100

    Condensed route and email files into 1.

--- app/api/send-verification/route.tsx
+++ app/api/send-verification/route.tsx
@@ -1,7 +1,25 @@
-import { sendVerifyEmail } from "@/lib/email";
+
+import { Resend } from "resend"
+import { VerifyEmail } from "@/lib/server/emails/VerifyEmail"
+
 import { createClient } from "@/lib/supabase/server";
 import getProfile from "@/lib/getProfile";
 import { generateVerifyToken } from "@/lib/generateVerifyToken";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendVerifyEmail({email, displayName, token} : {email: string, displayName: string, token: string}) {
+    if (email.length == 0) return {}
+
+    const verifyURL = `${process.env.NEXT_PUBLIC_SITE_URL}/account/verify?token=${token}`;
+
+    await resend.emails.send({
+        from: "Omni <onboarding@resend.dev>",
+        to: email,
+        subject: "Verify Your Email",
+        react: <VerifyEmail displayName={displayName} URL={verifyURL} />
+    })
+} 
 
 export async function POST() {
 
