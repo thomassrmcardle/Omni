79857a1f6ba7de874809d7c7f2d228a81c23498e
Thomas <thomas_mcardle@outlook.com>
2026-04-13 20:06:13 +0100

    Finally fixed email api (hopefully).

--- app/api/send-verification/route.ts
+++ app/api/send-verification/route.ts
@@ -1,9 +1,9 @@
 
 export const runtime = 'nodejs';
 
-import { createClient } from "@/lib/supabase/server";
-import getProfile from "@/lib/getProfile";
-import { generateVerifyToken } from "@/lib/generateVerifyToken";
+import { createClient } from "../../lib/supabase/server";
+import getProfile from "../../lib/getProfile";
+import { generateVerifyToken } from "../../lib/generateVerifyToken";
 
 export async function sendVerifyEmail({email, displayName, token} : {email: string, displayName: string, token: string}) {
     if (email.length == 0) return {}
@@ -14,21 +14,21 @@ export async function sendVerifyEmail({email, displayName, token} : {email: stri
     const verifyURL = `${process.env.NEXT_PUBLIC_SITE_URL}/account/verify?token=${token}`;
 
     const htmlContent = `
-<!DOCTYPE html>
-<html>
-<body style="font-family: Arial, sans-serif;">
-<div style="padding: 20px;">
-<p>Hello ${displayName},</p>
-<p>This email address has been linked to an Omni account. To verify this was you, please click the button below to complete the process.</p>
-<a href="${verifyURL}" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: white; text-decoration: none;">Verify Email</a>
-<p>If this was not you, please ignore this email. We apologise for any inconvenience.</p>
-</div>
-<div style="background-color: rgb(10,10,10); padding: 12px; width: 100%;">
-<p style="font-size: 12px; color: #888;">© ${new Date().getFullYear()} Omni. All rights reserved.</p>
-</div>
-</body>
-</html>
-`;
+        <!DOCTYPE html>
+        <html>
+        <body style="font-family: Arial, sans-serif;">
+        <div style="padding: 20px;">
+        <p>Hello ${displayName},</p>
+        <p>This email address has been linked to an Omni account. To verify this was you, please click the button below to complete the process.</p>
+        <a href="${verifyURL}" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: white; text-decoration: none;">Verify Email</a>
+        <p>If this was not you, please ignore this email. We apologise for any inconvenience.</p>
+        </div>
+        <div style="background-color: rgb(10,10,10); padding: 12px; width: 100%;">
+        <p style="font-size: 12px; color: #888;">© ${new Date().getFullYear()} Omni. All rights reserved.</p>
+        </div>
+        </body>
+        </html>
+    `;
 
     await resend.emails.send({
         from: "Omni <onboarding@resend.dev>",
