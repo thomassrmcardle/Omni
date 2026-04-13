0ee71c2ef1eca402af102a390cec35d2c88e2c4a
Thomas <thomas_mcardle@outlook.com>
2026-04-13 20:36:33 +0100

    Tried to resolve api import issue.

--- app/lib/getProfile.ts
+++ app/lib/getProfile.ts
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "./supabaseClient.js";
 
 export default async function getProfile(userId: string, retries = 3) {
   if (!userId) return null;
