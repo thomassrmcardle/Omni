801947def0d4d8a52ff4d9592b40308d3d200352
Thomas <thomas_mcardle@outlook.com>
2026-04-15 00:51:58 +0100

    Added section type support to editor.

--- app/app/post/create/page.tsx
+++ app/app/post/create/page.tsx
@@ -2,12 +2,60 @@
 
 import { useState } from "react"
 
+type Section = {
+    id: number
+    type: 'text' | 'quote'
+    content: string
+}
+
 export default function ArticleEditor() {
 
-    var [sections, setSections] = useState<string[]>([])
+
+    var [headline, SetHeadline] = useState("New Article")
+
+    var [sections, setSections] = useState<Section[]>([])
 
     const addSection = () => {
-        setSections(prev => [...prev, 'Test Section'])
+        setSections(prev => [
+            ...prev,
+            {id: Date.now(), type: 'text', content: '' },
+        ])
+    }
+
+    const updateSection = (id : number, field : keyof Section, value: string) => {
+        setSections(prev =>
+            prev.map(section => 
+                section.id == id ? {...section, [field]: value} : section
+            )
+        )
+    }
+
+    const removeSection = (id: number) => {
+        setSections(prev => prev.filter(section => section.id != id))
+    }
+
+
+    function SectionStack() {
+        return <div>{sections.map((section : any) => (<div>
+                <div key={section.id}>
+                    <select
+                        value={section.type}
+                        onChange={e => updateSection(section.id, "type", e.target.value)}
+                    >
+                        <option value="text">Text</option>
+                        <option value="Quote">Quote</option>
+                    </select>
+
+                    <textarea
+                        value={section.content}
+                        onChange={e => updateSection(section.id, "content", e.target.value)}
+                    />
+
+                    <button
+                        onClick={() => removeSection(section.id)}
+                    >Delete</button>
+                </div>
+            </div>))}</div>
     }
 
     function ArticleEditor() {
@@ -19,18 +67,16 @@ export default function ArticleEditor() {
         // Delete button to remove sections
 
         return <div>
-            {sections.map((section : any) => (<div>
-                <p>{section}</p>
-            </div>))}
+            <SectionStack />
             <button className="w-full" onClick={addSection}>
                 Add Section
             </button>
         </div>
     }
 
-    return <div className="bg-white dark:bg-black py-32 px-16 w-full flex flex-col">
+    return <div className="bg-white dark:bg-black py-32 px-16 w-full flex flex-col items-center">
         <div className="card w-full max-w-2xl flex flex-col">
-            <input name="article_headline" placeholder="Headline" value={"New Article"} />
+            <input name="article_headline" placeholder="Headline" value={headline} onChange={e => SetHeadline(e.target.value)} />
             <input name="article_subheadline" placeholder="Subheadline" />
         </div>
         <ArticleEditor />
